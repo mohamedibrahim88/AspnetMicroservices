28bbf5d58196c37f9a33e299e9ddb32d9dbfd926
mohamed ibrahim <mohamedkoory88@gmail.com>
2022-11-15 19:04:57 +0200

    Basket Microservices

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration config) { 
+        var client = new MongoClient(config.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var database = client.GetDatabase(config.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            Products = database.GetCollection<Prodcut>(config.GetValue<string>("DatabaseSettings:CollectionName"));
+
+            CatalogContextSeed.SeedData(Products)
+
+        }
+        public IMongoCollection<Prodcut> Products { get; }
+    }
+}
